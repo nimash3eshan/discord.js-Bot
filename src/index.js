89cd5f323156e86af9cb1dbf8d71f37b5e4bedf8
nimash eshan <81243419+nimash3eshan@users.noreply.github.com>
2022-12-15 12:10:06 +0530

    discord slash choices

--- src/index.js
+++ src/index.js
@@ -1,61 +1,101 @@
-import {Client, GatewayIntentBits, Routes} from 'discord.js';
-import {config} from 'dotenv';
-import {REST} from '@discordjs/rest'
+import { Client, GatewayIntentBits, Routes } from "discord.js";
+import { config } from "dotenv";
+import { REST } from "@discordjs/rest";
 
 config();
 
 const client = new Client({
   intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent
-  ]
-}); 
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 const TOKEN = process.env.BOT_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
-const rest = new REST({version:'10'}).setToken(TOKEN)
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on("messageCreate", (message) => {
+  console.log(message.content);
+});
 
-client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`)});
-client.on('messageCreate', message => {console.log(message.content)});
-
-client.on('interactionCreate', interaction => {
-  if(interaction.isChatInputCommand()) {
-    interaction.reply(`your ${interaction.options.getString('food')} order is preparing`);
+client.on("interactionCreate", (interaction) => {
+  if (interaction.isChatInputCommand()) {
+    const food = interaction.options.get("food").value;
+    const drink = interaction.options.get("drink").value;
+    interaction.reply(
+      `your ${food} order & ${drink} order is preparing`
+    );
   }
 });
 
 async function main() {
-
   const commands = [
     {
-      name: 'order',
-      description: 'order something',
+      name: "order",
+      description: "order something",
       options: [
         {
-          name: 'food',
-          description: 'The type of food you want to order',
+          name: "food",
+          description: "The type of food you want to order",
           type: 3,
-          required: true
+          required: true,
+          choices: [
+            {
+              name: "pizza",
+              value: "pizza"
+            },
+            {
+              name: "burger",
+              value: "burger"
+            },
+            {
+              name: "pasta",
+              value: "pasta"
+            }
+          ]
+        },
+        {
+          name: "drink",
+          description: "The type of drink you want to order",
+          type: 3,
+          required: false,
+          choices: [
+            {
+              name: "coke",
+              value: "coke"
+            },
+            {
+              name: "fanta",
+              value: "fanta"
+            },
+            {
+              name: "sprite",
+              value: "sprite"
+            }
+          ]
         }
       ]
-    },
-  ]; 
+    }
+  ];
 
-  try{
-    console.log('Started refreshing application (/) commands.');
-    await rest.put(Routes.applicationCommands(CLIENT_ID, GUILD_ID), { body: commands });
-    console.log('Successfully reloaded application (/) commands.');
-  }
-  catch(error) {
-    console.error(error)
+  try {
+    console.log("Started refreshing application (/) commands.");
+    await rest.put(Routes.applicationCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
   }
 }
 
 main();
 
-
-client.login(TOKEN);  
+client.login(TOKEN);
