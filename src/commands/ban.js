a0124fe94ab33563f3fb365dbed5f353793e7448
nimash eshan <81243419+nimash3eshan@users.noreply.github.com>
2022-12-17 20:27:30 +0530

    subcommand groups

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -3,27 +3,55 @@ import { SlashCommandBuilder } from "discord.js";
 const banCommand = new SlashCommandBuilder()
   .setName("ban")
   .setDescription("ban a user")
-  .addSubcommand((subcommand) =>
-    subcommand
+  // .addSubcommand((subcommand) =>
+  //   subcommand
+  //     .setName("temp")
+  //     .setDescription("temporary ban a user")
+  //     .addUserOption((option) =>
+  //       option
+  //         .setName("user")
+  //         .setDescription("The user you want to ban")
+  //         .setRequired(true)
+  //     )
+  // )
+  // .addSubcommand((subcommand) =>
+  //   subcommand
+  //     .setName("perm")
+  //     .setDescription("permanently ban a user")
+  //     .addUserOption((option) =>
+  //       option
+  //         .setName("user")
+  //         .setDescription("The user you want to ban")
+  //         .setRequired(true)
+  //     )
+  // );
+  .addSubcommandGroup((group) =>
+    group
       .setName("temp")
       .setDescription("temporary ban a user")
-      .addUserOption((option) =>
-        option
+      .addSubcommand((subcommand) =>
+        subcommand
           .setName("user")
           .setDescription("The user you want to ban")
-          .setRequired(true)
+          .addUserOption((option) =>
+            option
+              .setName("user")
+              .setDescription("The user you want to ban")
+              .setRequired(true)
+          )
       )
-  )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName("perm")
-      .setDescription("permanently ban a user")
-      .addUserOption((option) =>
-        option
-          .setName("user")
-          .setDescription("The user you want to ban")
-          .setRequired(true)
+      .addSubcommand((subcommand) =>
+        subcommand
+          .setName("perm")
+          .setDescription("permanently ban a user")
+          .addUserOption((option) =>
+            option
+              .setName("user")
+              .setDescription("The user you want to ban")
+              .setRequired(true) 
+          )
       )
   );
 
-export default channelCommand.toJSON();
+
+export default banCommand.toJSON();
